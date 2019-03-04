903f52f9b245e4d3997488d5e87c0a6617fd32a9
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-03-03 22:54:34 -0500

    Made it to flowcontrol 1

--- hello.go
+++ hello.go
@@ -3,15 +3,21 @@ package main
 
 import (
     "fmt" //'formatted IO'
-    "math"
 )
 
+const (
+    Big = 1 << 100
+    Small = Big >> 99
+)
+
+func needInt(x int) int {return x*10 + 1 }
+func needFloat(x float64) float64 {
+    return x * .1
+}
+
 func main() {
-    var x, y int = 3,4
-    var f float64 = math.Sqrt(float64(x*x + y*y))
-    var z uint = uint(f)
-    i := 42
-    j := float64(i)
-    k := uint(j)
-    fmt.Println(x,y,z)
+    fmt.Println(needInt(Small))
+    fmt.Println(needFloat(Small))
+    fmt.Println(needFloat(Big))
+    fmt.Println(needInt(Big))
 }
