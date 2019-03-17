d70325164a7886678d17ea701582f43867a86937
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-03-17 13:26:59 -0400

    Made it to moretypes 14 blog post

--- hello.go
+++ hello.go
@@ -6,21 +6,22 @@ import (
 )
 
 func main() {
-    a := make([]int, 5)
-    printSlice("a", a)
+    var s []int
+    printSlice(s)
 
-    b := make([]int, 0, 5)
-    printSlice("b", b)
-    
-    c := b[:2]
-    printSlice("c", c)
+    // append works on nil slices
+    s = append(s, 0)
+    printSlice(s)
 
-    d := c[2:5]
-    printSlice("d", d)
+    // the slice grows as needed
+    s = append(s, 1)
+    printSlice(s)
+
+    // add more than one element at a time
+    s = append(s, 2, 3, 4)
+    printSlice(s)
 }
 
-func printSlice(s string, x []int) {
-    fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+func printSlice(s []int) {
+    fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
-
-
