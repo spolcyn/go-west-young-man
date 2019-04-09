1f70b3c605fa9ef4df1978e9a80a02b7b9e31402
Stephen Polcyn <stephen.polcyn@gmail.com>
2019-04-09 19:16:14 -0400

    Made it to moretypes 24

--- hello.go
+++ hello.go
@@ -2,21 +2,23 @@
 package main
 
 import (
-    "fmt" //'formatted IO'
+    //"fmt" //'formatted IO'
+    "golang.org/x/tour/wc"
+    "strings"
 )
 
+func WordCount(s string) map[string]int {
+
+    var fields []string = strings.Fields(s)
+    var words = make(map[string]int)
+
+    for i := 0; i < len(fields); i++ {
+        words[fields[i]]++
+    }
+
+    return words
+}
+
 func main() {
-    m := make(map[string]int)
-
-    m["Answer"] = 42
-    fmt.Println("The value:", m["Answer"])
-
-    m["Answer"] = 48
-    fmt.Println("The value:", m["Answer"])
-
-    delete(m, "Answer")
-    fmt.Println("The value:", m["Answer"])
-
-    v,ok := m["Answer"]
-    fmt.Println("The value:",v, "Present?", ok)
+    wc.Test(WordCount)
 }
